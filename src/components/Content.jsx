943d50fb1dbd06dbfb946a70b1025fa2dc695f0a
Mohammed Khalid <mohammedkhalid11@gmail.com>
2019-01-08 21:22:34 +0530

    Content Style Changes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
+import './Content.css';
+
 class Content extends Component {
   render() {
     return (
-      <div className="App"> Check
-      </div>
+      <React.Fragment>
+	      <div className="LeftPanel"> 
+	         <select>
+			  <option value="volvo">Volvo</option>
+			  <option value="saab">Saab</option>
+			  <option value="mercedes">Mercedes</option>
+			  <option value="audi">Audi</option>
+			</select> 
+	      </div>
+	      <div className="RightPanel">
+	      </div>
+	      <div className="SubnitButton"> <input type="submit" value="Submit" /> </div>
+      </React.Fragment>
     );
   }
 }
 
 export default Content;
+
