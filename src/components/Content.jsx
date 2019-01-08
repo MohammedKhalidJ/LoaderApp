4e3b569883a9c599411d8df62e8511f855abc95d
Mohammed Khalid <mohammedkhalid11@gmail.com>
2019-01-08 20:39:47 +0530

    Header Style changes

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="App"> Check
+      </div>
+    );
+  }
+}
+
+export default Content;
